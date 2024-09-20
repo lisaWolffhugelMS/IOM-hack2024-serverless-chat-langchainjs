286f9b4369907763868d2dd9d421349694a18562
lisaWolffhugelMS <89141325+lisaWolffhugelMS@users.noreply.github.com>
2024-09-20 15:29:53 +0000

    switching rg and adding logging for debugging on data file deployment

--- packages/api/src/functions/documents-post.ts
+++ packages/api/src/functions/documents-post.ts
@@ -1,5 +1,5 @@
 import fs from 'node:fs/promises';
-import { type HttpRequest, type HttpResponseInit, type InvocationContext, app } from '@azure/functions';
+import { HttpRequest, type HttpResponseInit, type InvocationContext, app } from '@azure/functions';
 import { AzureOpenAIEmbeddings } from '@langchain/openai';
 import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf';
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
@@ -28,60 +28,68 @@ export async function postDocuments(request: HttpRequest, context: InvocationCon
     // Type mismatch between Node.js FormData and Azure Functions FormData
     const file = parsedForm.get('file') as any as File;
     const filename = file.name;
+    context.log(`Processing document: ${filename}`);
 
     // Extract text from the PDF
     const loader = new PDFLoader(file, {
       splitPages: false,
     });
+
     const rawDocument = await loader.load();
-    rawDocument[0].metadata.source = filename;
+    context.log(`RawDocument: ${JSON.stringify(rawDocument, null, 2)}`); // Convert to string for logging
+    if (rawDocument.length > 0) {
+      rawDocument[0].metadata.source = filename;
 
-    // Split the text into smaller chunks
-    const splitter = new RecursiveCharacterTextSplitter({
-      chunkSize: 1500,
-      chunkOverlap: 100,
-    });
-    const documents = await splitter.splitDocuments(rawDocument);
-
-    // Generate embeddings and save in database
-    if (azureOpenAiEndpoint) {
-      const credentials = getCredentials();
-      const azureADTokenProvider = getAzureOpenAiTokenProvider();
-
-      // Initialize embeddings model and vector database
-      const embeddings = new AzureOpenAIEmbeddings({ azureADTokenProvider });
-      await AzureCosmosDBNoSQLVectorStore.fromDocuments(documents, embeddings, { credentials });
-    } else {
-      // If no environment variables are set, it means we are running locally
-      context.log('No Azure OpenAI endpoint set, using Ollama models and local DB');
-      const embeddings = new OllamaEmbeddings({ model: ollamaEmbeddingsModel });
-      const folderExists = await checkFolderExists(faissStoreFolder);
-      if (folderExists) {
-        const store = await FaissStore.load(faissStoreFolder, embeddings);
-        await store.addDocuments(documents);
-        await store.save(faissStoreFolder);
-      } else {
-        const store = await FaissStore.fromDocuments(documents, embeddings, {});
-        await store.save(faissStoreFolder);
-      }
-    }
-
-    if (storageUrl && containerName) {
-      // Upload the PDF file to Azure Blob Storage
-      context.log(`Uploading file to blob storage: "${containerName}/${filename}"`);
-      const credentials = getCredentials();
-      const blobServiceClient = new BlobServiceClient(storageUrl, credentials);
-      const containerClient = blobServiceClient.getContainerClient(containerName);
-      const blockBlobClient = containerClient.getBlockBlobClient(filename);
-      const buffer = await file.arrayBuffer();
-      await blockBlobClient.upload(buffer, file.size, {
-        blobHTTPHeaders: { blobContentType: 'application/pdf' },
+      // Split the text into smaller chunks
+      const splitter = new RecursiveCharacterTextSplitter({
+        chunkSize: 1500,
+        chunkOverlap: 100,
       });
-    } else {
-      context.log('No Azure Blob Storage connection string set, skipping upload.');
+      const documents = await splitter.splitDocuments(rawDocument);
+
+      // Generate embeddings and save in database
+      if (azureOpenAiEndpoint) {
+        const credentials = getCredentials();
+        const azureADTokenProvider = getAzureOpenAiTokenProvider();
+
+        // Initialize embeddings model and vector database
+        const embeddings = new AzureOpenAIEmbeddings({ azureADTokenProvider });
+        await AzureCosmosDBNoSQLVectorStore.fromDocuments(documents, embeddings, { credentials });
+      } else {
+        // If no environment variables are set, it means we are running locally
+        context.log('No Azure OpenAI endpoint set, using Ollama models and local DB');
+        const embeddings = new OllamaEmbeddings({ model: ollamaEmbeddingsModel });
+        const folderExists = await checkFolderExists(faissStoreFolder);
+        if (folderExists) {
+          const store = await FaissStore.load(faissStoreFolder, embeddings);
+          await store.addDocuments(documents);
+          await store.save(faissStoreFolder);
+        } else {
+          const store = await FaissStore.fromDocuments(documents, embeddings, {});
+          await store.save(faissStoreFolder);
+        }
+      }
+
+      if (storageUrl && containerName) {
+        // Upload the PDF file to Azure Blob Storage
+        context.log(`Uploading file to blob storage: "${containerName}/${filename}"`);
+        const credentials = getCredentials();
+        const blobServiceClient = new BlobServiceClient(storageUrl, credentials);
+        const containerClient = blobServiceClient.getContainerClient(containerName);
+        const blockBlobClient = containerClient.getBlockBlobClient(filename);
+        const buffer = await file.arrayBuffer();
+        await blockBlobClient.upload(buffer, file.size, {
+          blobHTTPHeaders: { blobContentType: 'application/pdf' },
+        });
+      } else {
+        context.log('No Azure Blob Storage connection string set, skipping upload.');
+      }
+
+      return ok({ message: 'PDF file uploaded successfully.' });
     }
 
-    return ok({ message: 'PDF file uploaded successfully.' });
+    context.log('RawDocument is empty.');
+    return ok({ message: ` Error when processing document-post request: RawDocument is empty for ${filename}` });
   } catch (_error: unknown) {
     const error = _error as Error;
     context.error(`Error when processing document-post request: ${error.message}`);
